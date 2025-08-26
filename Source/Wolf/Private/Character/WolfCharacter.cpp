cc1df0d617898224182cc76a5c8f6e370eda28df
MishaSen <mishasen245@gmail.com>
2025-08-26 13:47:58 +0800

    Default Abilities

--- Source/Wolf/Private/Character/WolfCharacter.cpp
+++ Source/Wolf/Private/Character/WolfCharacter.cpp
@@ -10,13 +10,49 @@ AWolfCharacter::AWolfCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 	ASC = CreateDefaultSubobject<UWolfAbilitySystemComponent>(TEXT("Ability System Component"));
 	AtSet = CreateDefaultSubobject<UWolfAttributeSet>(TEXT("Attribute Set"));
+
+	/* Multiplayer would need to replicate ASC. Replication Mode either Mixed or Full.
+	 * 
+	 * ASC = CreateDefaultSubobject<UWolfAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	 * ASC->SetIsReplicated(true);
+	 * ASC->SetReplicationMode(EGameplayEffect ReplciationMode::Mixed);
+	 */
 }
 
 // Called when the game starts or when spawned
 void AWolfCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (ASC && DefaultAttributes)
+	{
+		FGameplayEffectContextHandle EffectContext = ASC->MakeEffectContext();
+		EffectContext.AddSourceObject(this);
+
+		FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(DefaultAttributes, 1, EffectContext);
+		if (SpecHandle.IsValid())
+		{
+			ASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), ASC);
+		}
+	}
+
+	if (HasAuthority() && ASC)
+	{
+		for (TSubclassOf<UGameplayAbility> AbilityClass : DefaultAbilities)
+		{
+			ASC->GiveAbility(FGameplayAbilitySpec(AbilityClass, 1, 0, this));
+		}
+	}
+
+	/* Input Binding (maybe future use)
+	 *
+	 * FGameplayAbilityInputBinds(
+	 * "Confirm",
+	 * "Cancel",
+	 * "EAbilityInputID",
+	 * static_cast<int32>(EAbilityInputID::Confirm),
+	 * static_cast<int32(EAbilityInputID::Cancel)));
+	 */
 }
 
 // Called every frame
@@ -31,5 +67,10 @@ void AWolfCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+UAbilitySystemComponent* AWolfCharacter::GetAbilitySystemComponent() const
+{
+	return ASC;
 }
 
